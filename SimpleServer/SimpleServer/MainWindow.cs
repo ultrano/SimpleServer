459fe37906293b83ea33781f7675ad6cf9ce12f8
Sangwoon Han <storypalse@gmail.com>
2017-02-12 20:54:47 +0900

    에러 수정.

--- SimpleServer/SimpleServer/MainWindow.cs
+++ SimpleServer/SimpleServer/MainWindow.cs
@@ -140,7 +140,8 @@ public class Player
 
 	public void SendAsync(byte[] buf)
 	{
-		Buffer.BlockCopy(buf, 0, sendBuf, 0, sendBuf.Length);
+		int length = Math.Min(sendBuf.Length, buf.Length);
+		Buffer.BlockCopy(buf, 0, sendBuf, 0, length);
 
 		if (!socket.SendAsync(recvArgs))
 			ProcessSend(recvArgs);
